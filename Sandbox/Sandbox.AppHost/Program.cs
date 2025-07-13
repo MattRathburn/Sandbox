4f137fe4b1a3230e366d9575dba47522bc0045a8
MattRathburn <m.rathburn@outlook.com>
2025-07-12 20:54:35 -0400

    added keycloak aspire and migrations proj

--- Sandbox/Sandbox.AppHost/Program.cs
+++ Sandbox/Sandbox.AppHost/Program.cs
@@ -1,4 +1,27 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var keycloak = builder.AddKeycloak("keycloak", 8080)
+    .WithDataVolume();
+
+var postgres = builder.AddPostgres("postgres")
+    .WithPgAdmin();
+
+var db = postgres.AddDatabase("sandbox-db");
+
+var migrations = builder.AddProject<Projects.Sandbox_Migrations>("sandbox-migrations")
+    .WithReference(db)
+    .WaitFor(postgres)
+    .WaitFor(db);
+
+if(builder.Environment.IsDevelopment())
+{
+    migrations.WithHttpCommand(path: "/reset-db", displayName: "Reset Database", commandOptions: new HttpCommandOptions
+    {
+        IconName = "fa-solid fa-database",
+    });
+}
 
 builder.AddProject<Projects.Sandbox_API>("sandbox-api");
 
